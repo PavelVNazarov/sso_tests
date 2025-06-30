89f06e4130125762c73a5527927e940edf96b4d1
PavelVNazarov <19pavel76@mail.ru>
2025-06-30 14:35:23 +0500

    Update conftest.py

--- sso_tests/conftest.py
+++ sso_tests/conftest.py
@@ -1,10 +1,14 @@
 # conftest.py
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    # Можно заменить на Firefox, если нужно
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     yield driver
     driver.quit()
 
